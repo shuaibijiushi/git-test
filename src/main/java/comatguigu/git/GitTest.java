ded246a31edb65e5f4ac7dd5b2ed03afab7731ff
fjh <975064072@qq.com>
2021-08-27 20:37:06 +0800

    second commit

--- src/main/java/comatguigu/git/GitTest.java
+++ src/main/java/comatguigu/git/GitTest.java
@@ -3,5 +3,7 @@ package comatguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1!");
+
     }
 }
